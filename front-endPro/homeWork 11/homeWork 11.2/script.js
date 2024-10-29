3beb0f09b8688d6a742bd7ea7945d3d87efef4e9
O.Katorzhyn <a.katorzhin@telesens.ua>
2024-10-29 13:10:29 +0200

    changed homework 11.2

--- front-endPro/homeWork 11/homeWork 11.2/script.js
+++ front-endPro/homeWork 11/homeWork 11.2/script.js
@@ -1,24 +1,6 @@
 const textBlock = document.getElementById("text-block");
 const toggleButton = document.getElementById("color-toggle-button");
 
-let isOriginalColor = true;
-const originalColor = textBlock.style.color;
-
-function getRandomColor() {
-  const letters = "0123456789ABCDEF";
-  let color = "#";
-  for (let i = 0; i < 6; i++) {
-    color += letters[Math.floor(Math.random() * 16)];
-  }
-  return color;
-}
-
 toggleButton.addEventListener("click", function () {
-  if (isOriginalColor) {
-    textBlock.style.color = getRandomColor();
-  } else {
-    textBlock.style.color = originalColor;
-  }
-
-  isOriginalColor = !isOriginalColor;
+    textBlock.classList.toggle("toggled-color");
 });

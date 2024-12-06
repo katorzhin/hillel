c28252ad62f66a8e7a471f5fdde6ff2dba3709e4
O.Katorzhyn <alex.katorzhin@gmail.com>
2024-12-06 16:12:05 +0200

    updated homeWork 20

--- front-endPro/homeWork 20/script.js
+++ front-endPro/homeWork 20/script.js
@@ -1,36 +1,61 @@
 $(document).ready(function () {
-    const $taskInput = $('#task-input');
+    const $taskInput = $('.js--form__input');
+    const $todoList = $('#todo-list');
+    const $taskTextModal = $('#task-text');
+    const taskModal = new bootstrap.Modal($('#taskModal'));
 
     function addTask() {
         const taskText = $taskInput.val().trim();
-        if (taskText) {
 
-            const listItem = document.createElement('li');
-
-            listItem.textContent = taskText;
-            listItem.classList.add('list-group-item', 'task-item');
-
-            document.querySelector('#todo-list').appendChild(listItem);
-
-            $taskInput.val('');
-        } else {
-            alert('Please enter a task!');
+        if (!taskText) {
+            alert('Введите задачу!');
+            return;
         }
+
+        const $taskItem = $('<li>')
+            .addClass('list-group-item todo-item d-flex justify-content-between align-items-center');
+
+        const $taskText = $('<span>')
+            .addClass('todo-item__description')
+            .text(taskText);
+
+        const $checkbox = $('<input>')
+            .attr('type', 'checkbox')
+            .addClass('todo-item__checkbox me-2')
+            .on('change', function () {
+                $taskText.toggleClass('text-decoration-line-through');
+            });
+
+        const $deleteButton = $('<button>')
+            .addClass('btn btn-danger btn-sm todo-item__delete')
+            .text('Видалити')
+            .on('click', function () {
+                $taskItem.remove();
+            });
+
+        $taskItem.on('click', function (e) {
+            if (!$(e.target).is('button, input')) {
+                $taskTextModal.text(taskText);
+                taskModal.show();
+            }
+        });
+
+        $taskItem.prepend($checkbox);
+        $taskItem.append($taskText);
+        $taskItem.append($deleteButton);
+
+        $todoList.append($taskItem);
+
+
+        $taskInput.val('');
     }
 
     $('#add-task').on('click', addTask);
 
     $taskInput.on('keydown', function (e) {
         if (e.key === 'Enter') {
-            addTask();
             e.preventDefault();
+            addTask();
         }
-    });
-
-    $(document).on('click', '.task-item', function () {
-        const taskText = $(this).text();
-        $('#task-text').text(taskText);
-        const modal = new bootstrap.Modal($('#taskModal'));
-        modal.show();
     });
 });
